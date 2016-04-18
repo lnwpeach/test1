693dbe98db74f86e88e94ee82ee1e43207e80ac1
peach <pichayuth.c@gmail.com>
2016-04-18 09:18:36 +0700

    111

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
 import random
+import calendar
 num = [1,2,3,4,5]
 num1 = random.randrange(101)
-print (num1)
+print ('Here is Calendar:\n')
+print (calendar.month(2016,4))
--- test1.py
+++ test1.py
@@ -1,5 +1,5 @@
-print 'This is a multiplication table\n'
+print ('This is a multiplication table\n')
 x = input('Enter X : ')
 for i in range(1,13):
     total = x*i
-    print(str(x)+' * '+str(i)+' = '+str(total))
+    print (x+' * '+i+' = '+total)
